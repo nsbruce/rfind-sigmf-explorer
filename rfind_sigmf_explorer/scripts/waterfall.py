17955daa7189c8df767b92201bd0f01745781616
Nicholas Bruce <saltair93+github@gmail.com>
2022-11-11 22:59:01 +0000

    Fix script

--- rfind_sigmf_explorer/scripts/waterfall.py
+++ rfind_sigmf_explorer/scripts/waterfall.py
@@ -6,15 +6,18 @@ from rfind_sigmf_explorer.waterfall_plotter import plot_interactive_waterfall
 
 
 def make_interactive_waterfall(args: Namespace) -> None:
-    sigmf_path = Path(args.sigmf_path)
+    sigmf_path = Path(args.sigmf_meta)
 
     samples = load_sigmf_full(sigmf_path)
 
     plot_interactive_waterfall(samples)
 
 
-if __name__ == '__main__':
+def main() -> None:
     parser = ArgumentParser()
     parser.add_argument('sigmf_meta', type=str, help="path to sigmf-meta file")
     args = parser.parse_args()
     make_interactive_waterfall(args)
+
+if __name__ == '__main__':
+    main()
